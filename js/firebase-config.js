9e5261ec7767ed5798704e7f4f2242c1945a4d59
Anahi1804 <lunarosadokeb@gmail.com>
2026-04-28 18:54:09 -0600

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,19 +1,17 @@
 // js/firebase-config.js
-// 1. Importamos las herramientas de Firebase directamente desde los servidores de Google
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getDatabase, ref, onValue, set, update } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
-// Si luego usan el login real, importaríamos la autenticación aquí también
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
-// 2. Las llaves de tu proyecto CloudPark
 const firebaseConfig = {
   apiKey: "AIzaSyB97Nlac70vu_rc_6XqCGPkfMa7rHCjkBk",
   databaseURL: "https://cloudpark-bcc27-default-rtdb.firebaseio.com/",
   projectId: "cloudpark-bcc27" 
 };
 
-// 3. Inicializamos la conexión
+// Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
+const auth = getAuth(app);
 
-// 4. Exportamos la base de datos para que los otros archivos (.js) puedan usarla
-export { db, ref, onValue, set, update };
+export { db, auth, ref, onValue, set, update, signInWithEmailAndPassword };
