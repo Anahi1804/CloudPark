331234263116130c971665a86d48036b1b14ddf3
Anahi1804 <lunarosadokeb@gmail.com>
2026-04-30 13:30:38 -0600

    Include 'get' in Firebase database imports and exports
    Added 'get' function to database imports and exports.

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,18 @@
 // js/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getDatabase, ref, onValue, set, update } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+// 1. Agregamos "get" en esta línea
+import { getDatabase, ref, onValue, set, update, get } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB97Nlac70vu_rc_6XqCGPkfMa7rHCjkBk",
   databaseURL: "https://cloudpark-bcc27-default-rtdb.firebaseio.com/",
-  projectId: "cloudpark-bcc27" 
+  projectId: "cloudpark-bcc27"
 };
 
-// Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const auth = getAuth(app);
 
-export { db, auth, ref, onValue, set, update, signInWithEmailAndPassword };
+// 2. Agregamos "get" al final de esta lista para que otros archivos puedan usarlo
+export { db, auth, ref, onValue, set, update, get, signInWithEmailAndPassword };
